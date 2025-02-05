46190eafc91348b65b9478999e1c4dff1035a5b6
Sara <saraodqvist@outlook.com>
2025-02-05 14:16:42 +0100

    Update logger.js

--- backend/services/logger.js
+++ backend/services/logger.js
@@ -1,42 +1,26 @@
 
 const winston = require('winston');
+const { combine, timestamp, json, printf } = winston.format;
+const timestampFormat = 'MMM-DD-YYYY HH:mm:ss';
 
 
-const logger = winston.createLogger({
-    level: 'info',
-    format: winston.format.json(),
+const wLogger = winston.createLogger({
+    format: combine(
+        timestamp({ format: timestampFormat }),
+        json(),
+        printf(({ level, message, timestamp, ...data }) => {
+            const response = { level, message, data };
+            return JSON.stringify(response);
+        })
+    ),
+
     transports: [new winston.transports.Console()],
+    
   });
 
 
 
-
-
-/*
-const Log = require('./Model/schemas/logSchema.js');
-
-async function logEvent(serviceName, logLevel, message, metadata = {}) {
-    try {
-        const logEntry = new Log({
-            serviceName,
-            message,
-            metadata,
-            timestamp: new Date(),
-        });
-
-        await logEntry.save();
-        console.log( `[${logLevel}] ${message}`);
-    } catch (err) {
-        console.error('Failed to log event: ', err.message);
-    }
-}
-
-module.exports = {
-  logInfo: (serviceName, message, metadata) => logEvent(serviceName, 'INFO', message, metadata),
-  logError: (serviceName, message, metadata) => logEvent(serviceName, 'ERROR', message, metadata)
-};
-
- */
+module.exports = wLogger;
 
 
 
