b87ed5b3f59223db635f905e77c92d32f825aa6a
Sara <saraodqvist@outlook.com>
2025-02-05 14:58:58 +0100

    logs are now stored in the database
    logs are stored in the 'Logs' collection in mongoDB.

--- backend/services/logger.js
+++ backend/services/logger.js
@@ -1,21 +1,35 @@
 
 const winston = require('winston');
+
+require('winston-mongodb');
+
 const { combine, timestamp, json, printf } = winston.format;
 const timestampFormat = 'MMM-DD-YYYY HH:mm:ss';
+
+
+const logFormat = printf(({ level, message, timestamp, ...data }) => {
+    const response = { level, message, data };
+    return JSON.stringify(response);
+});
 
 
 const wLogger = winston.createLogger({
     format: combine(
         timestamp({ format: timestampFormat }),
         json(),
-        printf(({ level, message, timestamp, ...data }) => {
-            const response = { level, message, data };
-            return JSON.stringify(response);
-        })
+        logFormat
     ),
 
-    transports: [new winston.transports.Console()],
-    
+    transports: [
+        new winston.transports.Console(),
+        new winston.transports.MongoDB({
+            db: `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@booc.oduvk.mongodb.net/Booc?retryWrites=true&w=majority`,
+            options: { useUnifiedTopology: true },
+            collection: 'Logs',
+            level: 'info',
+        })
+    ],
+
   });
 
 
