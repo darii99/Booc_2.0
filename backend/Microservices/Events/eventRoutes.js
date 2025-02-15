00455ac91a9a7adf9f23e5381048563853c261b4
Sara <saraodqvist@outlook.com>
2025-02-15 14:04:01 +0100

    Event microservice is working

--- backend/Microservices/Events/eventRoutes.js
+++ backend/Microservices/Events/eventRoutes.js
@@ -2,40 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 
-const {authenicate, authStatus, removeAuth} = require("../../Controller/authController");
-const {getCurrentUser} = require('../Users/usersController');
-
-const {addFriend, deleteFriend} = require('../../Controller/friendController');
+const {userAuth} = require('../Users/usersController');
 const { createEvent, deleteEvent, getEvents } = require('../Events/eventController');
 
 
 //Authorization
-router.post("/auth", authenicate);
-//router.get("/auth", authStatus);
-//router.delete("/auth", removeAuth);
-
-//Users
-router.get("/users", getCurrentUser);
-//router.post("/users", createUser);
-//router.put("/users", changeStartPage);
-//router.delete("/users", deleteUser);
-
-//Friend
-//router.post("/users/addFriend", addFriend);
-//router.delete("/users/deleteFriend", deleteFriend);
-
-//Password
-//router.put("/password", changePassword);
-
-//Group
-//router.get("/group", getGroup);
-//router.post("/group", createGroup);
-//router.put("/group", updateGroup);
-//router.delete("/group", deleteGroup);
-
-//Groups
-//router.get("/groups", getAllGroups);
-//router.delete("/groups", leaveGroup);
+router.get("/auth", userAuth);
 
 //Events
 router.get("/event", getEvents);
@@ -47,12 +19,5 @@ router.delete("/event", deleteEvent);
 
 
 module.exports = router;
-
-
-
-
-
-
-
 
 
