476e6d0628a38213f35b94475218eace6aa65fa3
Sara <saraodqvist@outlook.com>
2025-03-05 15:05:04 +0100

    Update axiosTemplate.js

--- frontend/src/controllers/axiosTemplate.js
+++ frontend/src/controllers/axiosTemplate.js
@@ -2,10 +2,10 @@ import axios from 'axios';
 axios.defaults.withCredentials = true;
 
 export const api = axios.create({
-    baseURL: "http://9.223.173.7:4000",
+    baseURL: "http://9.223.169.94:4000",
     withCredentials: true,
     headers:{
-      "Access-Control-Allow-Origin": "http://9.223.173.7:4000",
+      "Access-Control-Allow-Origin": "http://9.223.169.94:4000",
       "Access-Control-Allow-Credentials":"true",
     }
   })
